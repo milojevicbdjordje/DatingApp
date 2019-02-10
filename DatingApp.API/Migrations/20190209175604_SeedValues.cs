96e9ec7ab154521dba842fb42a96245a1f00f150
Djordje Milojevic <milojevic.b.djordje@gmail.com>
2019-02-10 12:32:00 +0100

    Initial commit

---
+++ DatingApp.API/Migrations/20190209175604_SeedValues.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class SeedValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO 'Values' (Name) VALUES ('Value 101')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
