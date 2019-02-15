24237157140b038017c1f7ffcd5ea6dee854c86e
Djordje Milojevic <milojevic.b.djordje@gmail.com>
2019-02-15 16:53:17 +0100

    extending API

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(() => {
       this.alertify.success('registration successful');
     }, error => {
-      this.alertify.error(error);
+       this.alertify.error(JSON.stringify(error));
     });
   }
 
